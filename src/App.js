7e29013f1b8c1e9e2438a82c81ff7c56d0cbe116
Aashish Panthi <60884239+aashishpanthi@users.noreply.github.com>
2021-11-20 09:23:02 +0545

    files by Roshan

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Navbar from "./compoent/navbar.js"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from "./compoent/Home.js";
+import Destinations from "./compoent/Destinations.js";
+import Accomodation from "./compoent/Accomodation.js";
+import Experience from "./compoent/Experience.js";
+import Blog from "./compoent/Blog.js";
+import Intro from "./compoent/Intro.js";
+import "./compoent/navbar.css";
+import Bottom from "./compoent/bottom.js"
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+          <Route path="/destinations" component={Destinations} ></Route>
+          <Route path="/accomodation" component={Accomodation} ></Route>
+          <Route path="/experiences" component={Experience} ></Route>
+          <Route path="/blog" component={Blog} ></Route>
+          </Switch>
+      <Intro/>
+      <Destinations/>
+      <Accomodation/>
+      <Bottom/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
